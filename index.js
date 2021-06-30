bb9f2209dfff8b8936403ae8e0a16853f4dd3087
Radensuaso <andrels9283@gmail.com>
2021-06-30 14:18:09 +0100

    Back Office form done, need to add functionality

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
   },
 })
  */
+
+const displayItems = async (url) => {
+  const response = await fetch(url, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNjA5ZmIzNTgxNzAwMTVjMjI3MGMiLCJpYXQiOjE2MjUwNTUzOTEsImV4cCI6MTYyNjI2NDk5MX0.4rreCWruc8iinYHIIdhbPTQo52bs9c82UeMWN-fKg0o",
+    },
+  })
+
+  const items = await response.json()
+  console.log(items)
+}
+
+window.onload = () => {
+  const url = "https://striveschool-api.herokuapp.com/api/product/"
+
+  displayItems(url)
+}
